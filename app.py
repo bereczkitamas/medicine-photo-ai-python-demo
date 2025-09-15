acfdd48a729f9f105eda3070afd07b5acfad6f50
tamas_bereczki <tamas_bereczki@epam.com>
2025-09-15 12:09:15 +0200

    fixup! feat: replace Flask to FastAPI (WIP)

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host='0.0.0.0', port=int(os.environ.get('PORT', '8000')), reload=True)
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', '8000')))
